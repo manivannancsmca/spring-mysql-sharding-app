f7db771042a8838bb1ffc3b87f0c925b5cd0a778
manivannancsmca <manivannancsmca@gmail.com>
2026-07-15 16:55:09 +0530

    first commit

---
+++ src/main/java/com/spring_mysql_sharding_app/SpringMysqlShardingAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring_mysql_sharding_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlShardingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlShardingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_mysql_sharding_app/SpringMysqlShardingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_mysql_sharding_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlShardingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
